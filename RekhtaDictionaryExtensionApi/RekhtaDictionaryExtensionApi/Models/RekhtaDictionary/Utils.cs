4ffd6c0d15d72438b446e26c2cafb920af10b4f4
Bhavy Khatri <bhavykhatri@gmail.com>
2022-08-06 14:42:35 +0530

    Add .net code for migration from app service to function apps

---
+++ RekhtaDictionaryExtensionApi/RekhtaDictionaryExtensionApi/Models/RekhtaDictionary/Utils.cs
@@ -0,0 +1,23 @@
+﻿using RekhtaDictionary.Constants;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Models.RekhtaDictionary
+{
+    public static class Utils
+    {
+        public enum SupportedLanguage
+        {
+            English,
+            Hindi,
+            Urdu
+        }
+
+        public static IEnumerable<HtmlNode> GetNodesWithClassName(HtmlDocument document, string className)
+        {
+            return document.DocumentNode.Descendants(0).Where(n => n.HasClass(className));
+        }
+
+    }
+}
---
+++ RekhtaDictionaryExtensionApi/RekhtaDictionaryExtensionApi/TypeDefinition/DictionaryResponse.cs
@@ -0,0 +1,27 @@
+﻿using RekhtaDictionary.Constants;
+using HtmlAgilityPack;
+using Models.RekhtaDictionary;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RekhtaDictionaryAPI.TypeDefinition
+{
+    public enum ResponseStatus
+    {
+        NoResponse,
+        Ok
+    }
+    public class DictionaryResponse
+    {
+        public string Origin { get; set; }
+
+        public string Vazn { get; set; }
+
+        public Dictionary<Utils.SupportedLanguage, Meaning> MeaningByLanguage { get; set; }
+
+        public List<string> RelatedWords { get; set; }
+
+        public ResponseStatus Status { get; set; }
+
+    }
+}
